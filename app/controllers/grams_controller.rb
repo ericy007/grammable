25ebf86221da3c9ce0213401ff49b78eddf04d46
Eric Yabit <eric.yabit@live.com>
2019-09-22 03:37:39 +0000

    Validating grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -9,13 +9,16 @@ class GramsController < ApplicationController
 
   def create
     @gram = Gram.create(gram_params)
+    if @gram.valid?
     redirect_to root_path
-  end
+   else
+    render :new, status: :unprocessable_entity
+   end
+end
 
   private
 
   def gram_params
     params.require(:gram).permit(:message)
   end
-
 end
