d4fc7360ae177cf3e9ec389060dc171d57fef1f0
Eric Yabit <eric.yabit@live.com>
2019-09-23 05:11:07 +0000

    destroy grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,15 @@ class GramsController < ApplicationController
   def index
   end
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+      return render_not_found if @gram.blank?
+
+    @gram.destroy
+    redirect_to root_path
+  end
+
+
 
 def show
   @gram = Gram.find_by_id(params[:id])
